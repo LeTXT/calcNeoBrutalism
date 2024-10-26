c750d72dc4b93ad5ccbd077402319b1ea0771e17
Leandro <leandrogomesdeoliveira2@gmail.com>
2024-10-26 14:57:42 -0300

    add function addSymbol and istNumber, and add input readonly, centralized calculator

--- utility/calc.js
+++ utility/calc.js
@@ -2,6 +2,8 @@ const button = document.querySelectorAll("button")
 const input = document.querySelector("#input")
 const secundInput = document.querySelector("#secundInput")
 
+let calculated = false
+
 const calc = () => {
     console.log(input.value);
     console.log(parseFloat(input.value));
@@ -11,6 +13,7 @@ const calc = () => {
         const result = eval(input.value.replace(/÷/g, "/").replace(/×/g, "*"))
         secundInput.value = input.value
         input.value = result
+        calculated = true
     } catch (error) {
         console.error(error);
         input.value = "Error"
@@ -19,16 +22,44 @@ const calc = () => {
     
 }
 
+const itsNumber = (eNumber) => {
+    if(!calculated) {
+        input.value += eNumber;
+    } 
+    if(calculated) {
+        secundInput.value = input.value
+        input.value = eNumber
+        calculated = false
+    }
+}
+
+const addSymbol = (eSymbol) => {
+    
+    if(/[^\w\s]/.test(input.value.slice(-1))) {
+        input.value = input.value.slice(0, -1)
+        input.value += eSymbol;
+        calculated = false   
+    }
+    if(!/[^\w\s]/.test(input.value)) {
+        input.value += eSymbol
+        calculated = false
+    }
+    
+}
+
+
+
 button.forEach((btn) => {
     btn.addEventListener("click", (e) => {
 
         let valueNumber = e.target.textContent
 
         if(/^\d+(\.\d+)?$/.test(valueNumber)) { // Number
-            input.value += valueNumber;
+            // input.value += valueNumber;
+            itsNumber(valueNumber)
         }
-        if(/^[^\w\s]+$/.test(valueNumber) && !/[^\w\s]/.test(input.value)) { // Symbol
-            input.value += valueNumber;
+        if(/^[^\w\s]+$/.test(valueNumber)) { // Symbol
+            addSymbol(valueNumber)
         }
         if(valueNumber === "RESULT") {
             calc()
